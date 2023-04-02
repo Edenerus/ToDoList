d63720bfdb256d90bb30bbbd7123fd0f59bf5be7
Edenerus <markeev.25@mail.ru>
2023-04-02 18:45:23 +0300

    CI/CD

--- todolist/settings.py
+++ todolist/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': "django.db.backends.postgresql",
+        'ENGINE': env('DB_ENGINE'),
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),

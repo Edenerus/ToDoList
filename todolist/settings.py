02bc4bab2fcca546965e437a89cab945f5f42eb2
Edenerus <markeev.25@mail.ru>
2023-04-12 17:31:53 +0300

    fix dockerignore and settings

--- todolist/settings.py
+++ todolist/settings.py
@@ -20,12 +20,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = ['*']
-
-CSRF_TRUSTED_ORIGINS = ['https://edenerus.online:8001', ]
-CSRF_COOKIE_SECURE = True
 
 
 # Application definition
